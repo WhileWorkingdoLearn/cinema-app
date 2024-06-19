a857d32f95d7a87462f4ac43eab18cc70422ba08
s_ms_on_github <90606181+WhileWorkingdoLearn@users.noreply.github.com>
2024-06-19 19:03:41 +0000

    Update

--- src/Components/Slide/ViewVontroller.tsx
+++ src/Components/Slide/ViewVontroller.tsx
@@ -1,17 +1,19 @@
-
 export interface ViewController {
-    CardDim: {width:number,height:number};
-    AnimPercentage:number,
-    AnimTime:number,
-};
+  CardDim: { width: number; height: number };
+  AnimPercentage: number;
+  AnimTime: number;
+  calcIndex: (cardCount: number) => number;
+}
 
+export default function Controller(): ViewController {
+  const reCalcIndex = (cardCount: number) => {
+    return 0;
+  };
 
-export default function Controller():ViewController{
-   
-    
-    return {
-            CardDim:{width:150,height:150},
-            AnimPercentage:25,
-            AnimTime:400,
-    }
+  return {
+    CardDim: { width: 150, height: 150 },
+    AnimPercentage: 25,
+    AnimTime: 400,
+    calcIndex: reCalcIndex,
+  };
 }
